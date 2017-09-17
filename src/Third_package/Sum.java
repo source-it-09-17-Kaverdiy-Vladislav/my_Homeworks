32f88eed70963853ec7ed85afe16888622801541
Vladislav Kaverdiy <vladyslav.kaverdii@nure.ua>
2017-09-17 21:32:35 +0300

    Добавлена 2 часть дз

---
+++ src/Third_package/Sum.java
@@ -0,0 +1,13 @@
+package Third_package;
+
+public class Sum {
+    public static void main(String[] args) {
+        int sum ;
+        int x = 10;
+        int y = 20;
+
+       sum = x + y ;
+
+        System.out.println("Сумма двух чисел = " + sum);
+    }
+}
