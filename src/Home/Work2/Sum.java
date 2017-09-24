bc9094e0a5eea687c4013983bdf632b69d00f438
Vladislav Kaverdiy <vladyslav.kaverdii@nure.ua>
2017-09-24 15:02:37 +0300

    changed inputNumber and changed public on private

--- src/Home/Work2/Sum.java
+++ src/Home/Work2/Sum.java
@@ -2,15 +2,15 @@ package Home.Work2;
 
 public class Sum {
 
-    public static int inputNumber = 78531;
-    public static int sum = 0;
+    private static int inputNumber = 78531;
+    private static int sum = 0;
 
     public int getSumAllNumbers(){
 
    int x = inputNumber;
         while(inputNumber!= 0){
             sum = sum + inputNumber%10;
-            inputNumber /=10;
+            inputNumber = inputNumber/10;
         }
 
         System.out.println("The sum of the arguments " + x +" = " + sum);
